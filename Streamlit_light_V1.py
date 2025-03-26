5b634069296ba8564c1f25d79e148637ed93ffd0
Ivanne-P <ivanne@learninganimals.co>
2025-03-26 13:11:54 +0100

    Update Streamlit_light_V1.py
    Removed import plt and sns

--- Streamlit_light_V1.py
+++ Streamlit_light_V1.py
@@ -6,8 +6,8 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from plotly.subplots import make_subplots
 import plotly.express as px
 import io
